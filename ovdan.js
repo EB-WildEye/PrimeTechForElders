cd98b273c7d2e870f60f93bfc45df08dae6eb42e
Ein-Bar Surie <77734536+EB-WildEye@users.noreply.github.com>
2024-10-07 10:34:16 +0000

    Initial commit of QueenB project

---
+++ ovdan.js
@@ -0,0 +1,3 @@
+function homePage(){
+  location.replace("index.html")
+}
---
+++ script.js
@@ -0,0 +1,18 @@
+function nextPageN(){
+  location.replace("n.html")
+}
+function nextPageSec(){
+ location.replace("sec.html")                       
+}
+function nextPageOvdan(){
+location.replace("ovdan.html")
+}
+function nextPageEzra(){
+location.replace("ezra.html")
+}
+function nextPageBetihut(){
+location.replace("betihut.html")
+}
+
+
+
